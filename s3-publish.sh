76199717ca1edd7d48afde8f416807d0bbd65ece
Alexey Mukas <ritbl@protonmail.com>
2022-10-16 03:13:43 +0200

    wip

--- s3-publish.sh
+++ s3-publish.sh
@@ -2,3 +2,5 @@
 set -o xtrace
 
 find ./components/grafana -maxdepth 1 -name "*.tar.lz4" -exec aws s3 cp "{}" s3://pmm-x/ \;
+find ./components/exporters -maxdepth 1 -name "*.tar.lz4" -exec aws s3 cp "{}" s3://pmm-x/ \;
+find ./components/core -maxdepth 1 -name "*.tar.lz4" -exec aws s3 cp "{}" s3://pmm-x/ \;
