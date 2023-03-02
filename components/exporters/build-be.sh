cc709e073a4b1e3e1c52b7186259a6b90f1f9e4b
Alexey Mukas <ritbl@protonmail.com>
2023-03-02 09:50:30 +0100

    fixes

--- components/exporters/build-be.sh
+++ components/exporters/build-be.sh
@@ -7,7 +7,7 @@ cd /build
 
 ## -- azure_exporter
 cd ./deps/azure_metrics_exporter
-go build
+go build -buildvcs=false
 cd -
 
 # fixes build issue on jetbuild
@@ -15,20 +15,20 @@ go env -w GOFLAGS="-buildvcs=false"
 
 ## -- mongodb_exporter
 cd ./deps/mongodb_exporter
-go build
+go build -buildvcs=false
 cd -
 
 ## -- node_exporter
 cd ./deps/node_exporter
-go build
+go build -buildvcs=false
 cd -
 
 ## -- postgres_exporter
 cd ./deps/postgres_exporter/cmd/postgres_exporter
-go build
+go build -buildvcs=false
 cd -
 
 ## -- rds_exporter
 cd ./deps/rds_exporter
-go build
+go build -buildvcs=false
 cd -
