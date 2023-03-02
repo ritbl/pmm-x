11833daea51032727bea902bccb7f324db67078e
Alexey Mukas <ritbl@protonmail.com>
2023-03-02 11:51:14 +0100

    fixing core

--- components/core/build-be.sh
+++ components/core/build-be.sh
@@ -5,12 +5,8 @@ set -e
 echo "Building Core Backend"
 cd /build
 
-if [[ -z "${X_FIX_GIT_ACCESS}"] ]; then
-  echo "Fixing git access"
-  git config --global safe.directory '*'
-else
-  echo "Skipping fixing git access"
-fi
+#workaround for jetbuild... if put in IF block it doesn't work :(
+git config --global safe.directory '*'
 
 cd ./deps/pmm
 make init release
